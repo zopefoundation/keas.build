1dec5deb611a78afc5c134dbb319e35df6b1dbb7
Adam Groszer <agroszer@gmail.com>
2013-09-25 09:18:40 +0200

    Preparing release 0.3.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name='keas.build',
-    version='0.3.1dev',
+    version='0.3.1',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description='A Build System',
