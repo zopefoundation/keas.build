626f3bf7bb1b388433e2b2c3dcbd05517d6d1073
Adam Groszer <agroszer@gmail.com>
2013-09-25 09:19:04 +0200

    Back to development: 0.3.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name='keas.build',
-    version='0.3.1',
+    version='0.3.2.dev0',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description='A Build System',
